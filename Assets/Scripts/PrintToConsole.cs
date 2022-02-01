1d55df31100deb3d81d3134ff8f856b5df98d056
Alex Woods <alexanderwoods2019@gmail.com>
2022-02-01 14:40:51 -0500

    Added GameObjects and Scripts from course

---
+++ Assets/Scripts/PrintToConsole.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    #region Variables
+    // Variables.
+    #endregion
+
+    #region Unity Methods
+
+    void Start()
+    {
+        Debug.Log("Chello World!");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Private Methods
+    // Private Methods.
+    #endregion
+
+    #region Public Methods
+    // Public Methods
+    #endregion
+}
